208bb889f6fe6f5dbff171b2a19541eed593239c
PercyJackson235 <Percyjackson235@gmail.com>
2020-11-11 10:49:18 -0500

    development

--- pyftp.py
+++ pyftp.py
@@ -3,6 +3,9 @@ import cmd
 import socket
 import getpass
 import random
+import os
+import time
+import re
 
 
 class Client(cmd.Cmd):
@@ -17,15 +20,22 @@ class Client(cmd.Cmd):
         self.password = password
         self.control_sock = socket.socket()
         self.data_sock = None
+        self._port_cmd = False
         if host is not None:
             pass
             # self.do_open('')
             # self.do_user('')
-        self.run = False
+        self.running = False
 
     def do_fake(self, fake):
         """Fake method for testing"""
-        print(type(fake), fake)
+        print("Calling do_fake and fake")
+        a = self.fake(fake)
+        print(a)
+
+    def fake(self, fake):
+        print("Doing fake")
+        return fake[::-1]
 
     def do_user(self, arg: str = ''):
         """send new user information"""
@@ -43,7 +53,28 @@ class Client(cmd.Cmd):
 
     def do_cd(self, arg: str):
         """change remote working directory"""
-        print(f"CWD {arg}")
+        self.control_sock.send(bytes(f"CWD {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_lcd(self, arg: str):
+        """change local working directory"""
+        try:
+            os.chdir(arg.strip())
+        except FileNotFoundError:
+            print(f"{arg} does not exist.")
+        except NotADirectoryError:
+            print(f"{arg} is not a directory.")
+        except PermissionError:
+            print(f"Do not have permission to change directory to {arg}")
+
+    def do_shell(self, arg):
+        """escape to the shell"""
+        os.system(arg)
+
+    def do_cdup(self, arg):
+        """change remote working directory to parent directory"""
+        self.control_sock.send("CDUP\r\n".encode())
+        print(self.control_sock.recv(1024).decode())
 
     def do_open(self, arg=''):
         """connect to remote ftp server"""
@@ -66,7 +97,7 @@ class Client(cmd.Cmd):
         finally:
             print(self.host)
             print(self.port)
-        if self.run:
+        if self.running:
             self.control_sock.connect((self.host, self.port))
             print(self.control_sock.recv(1024).decode())
 
@@ -75,7 +106,7 @@ class Client(cmd.Cmd):
            calculated together."""
         try:
             port = int(port)
-        except ValueError:
+        except (ValueError, TypeError):
             pass
         if isinstance(port, int):
             while True:
@@ -83,17 +114,115 @@ class Client(cmd.Cmd):
                 temp = port - p2
                 if temp % 256 == 0:
                     p1 = temp >> 8
-                    return p1, p2
+                    return f"{p1},{p2}"
         else:
             try:
                 port = list(map(int, port))
                 return (port[0] << 8) + port[1]
             except ValueError as e:
-                print(str(e))
+                print(str(e))  # Need to setup proper exception handling back up the stack
+
+    def do_sendport(self, arg):
+        """toggle use of PORT cmd for each data connection. Not full implemented yet."""
+        if not self.running:
+            return
+        if arg != '':
+            if arg.lower() == 'on':
+                self._port_cmd = True
+            elif arg.lower() == 'off':
+                self._port_cmd = False
+        else:
+            self._port_cmd = not self._port_cmd
+        print("Using PORT cmd" if self._port_cmd else "Using PASV cmd")
+
+    def _create_data_conn(self):
+        if self._port_cmd:
+            self._create_sock(active_ftp=True)
+            time.sleep(1)
+            ipadder = str(self.control_sock.getsockname()[0]).replace('.', ',')
+            ipadder += ',' + self._portmath(self.control_sock.getsockname()[1])
+            self.control_sock.send(bytes(f"PORT {ipadder}\r\n"))
+            print(self.control_sock.recv(1024).decode())
+        else:
+            self.control_sock.send(b"PASV\r\n")
+            address = self.control_sock.recv(1024).decode().strip()
+            address = re.search(r'((\d{1,3},){4})(\d{1,3},\d{1,3})', address).groups()
+            port = (lambda x, y: (x * 256) + y)(*map(int, address[-1].split(',')))
+            self._create_sock(active_ftp=False, target=address[0], port=port)
+
+    def _create_sock(self, active_ftp: bool, target=None, port: int = 0):
+        self.data_sock = socket.socket()
+        if active_ftp:
+            while True:
+                try:
+                    self.data_sock.bind(('localhost', random.randint(40000, 60000)))
+                    break
+                except OSError:
+                    pass
+        else:
+            self.data_sock.connect((target, port))
+
+    def do_get(self, arg):
+        self.control_sock.send(bytes(f"RETR {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_binary(self, arg):
+        """set binary transfer type"""
+        self.control_sock.send(b'TYPE I\r\n')
+        print(self.control_sock.recv(1024).decode())
+
+    def do_image(self, arg):
+        """set binary transfer type"""
+        self.do_binary(None)
+
+    def do_ascii(self, arg):
+        """set ascii transfer type"""
+        self.control_sock.send(b'TYPE A\r\n')
+        print(self.control_sock.recv(1024).decode())
+
+    def do_ls(self, arg: str = ''):
+        """list contents of remote directory"""
+        self.control_sock.send(bytes(f"LIST {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_dir(self, arg: str = ''):
+        """list contents of remote directory"""
+        self.do_ls(arg)
+
+    def do_system(self, arg):
+        """get system information"""
+        self.control_sock.send(b"SYST\r\n")
+        print(self.control_sock.recv(1024).decode())
+
+    def do_bye(self, arg):
+        """terminate ftp session and exit"""
+        if self.running:
+            self.do_disconnect('')
+
+    def do_close(self, arg):
+        """terminate ftp session"""
+        self.control_sock.send(b"QUIT\r\n")
+        print(self.control_sock.recv(1024).decode())
+        self.control_sock.close()
+
+    def do_disconnect(self, arg):
+        """terminate ftp session"""
+        self.do_close('')
+
+    def do_delete(self, arg):
+        """delete remote file"""
+        self.control_sock.send(bytes(f"DELE {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_pwd(self, arg):
+        self.control_sock.send(b"PWD\r\n")
+        print(self.control_sock.recv(1024).decode())
 
     def do_quit(self, arg):
-        """Exiting the Program."""
+        """terminate ftp session"""
         print('QUIT')
+        if self.running:
+            self.do_disconnect('')
         return True
 
 
