d238442f8daf636c6d3977ac358c47aff3898471
PercyJackson235 <Percyjackson235@gmail.com>
2021-01-14 19:53:13 -0500

    Fixed file download

--- pyftp.py
+++ pyftp.py
@@ -2,10 +2,13 @@
 import cmd
 import socket
 import getpass
-import random
+from random import randint
 import os
 import time
 import re
+import tqdm
+import select
+import sys
 
 
 class Client(cmd.Cmd):
@@ -22,24 +25,15 @@ class Client(cmd.Cmd):
         self.data_sock = None
         self._port_cmd = False
         if host is not None:
-            pass
-            # self.do_open('')
-            # self.do_user('')
-        self.running = False
-
-    def do_fake(self, fake):
-        """Fake method for testing"""
-        print("Calling do_fake and fake")
-        a = self.fake(fake)
-        print(a)
-
-    def fake(self, fake):
-        print("Doing fake")
-        return fake[::-1]
+            self.do_open(host)
 
     def do_user(self, arg: str = ''):
         """send new user information"""
         # sent as USER user and PASS password
+        try:
+            self.control_sock.getpeername()
+        except OSError:
+            return
         if arg == '':
             self.user = getpass.getuser()
             temp = input(f"Name ({self.host}:{self.user}): ").strip()
@@ -47,9 +41,15 @@ class Client(cmd.Cmd):
                 self.user = temp
         else:
             self.user = arg
-        self.password = getpass.getpass(prompt='Password: ').strip()
-        print(f"USER {self.user}")
-        print(f"PASS {self.password}")
+        self.control_sock.send(bytes(f"USER {self.user}\r\n", 'utf-8'))
+        resp = self.control_sock.recv(1024).decode()
+        print(resp)
+        if resp.startswith('3'):
+            self.password = getpass.getpass(prompt='Password: ').strip()
+            self.control_sock.send(bytes(f"PASS {self.password}\r\n", 'utf-8'))
+            print(self.control_sock.recv(1024).decode())
+        else:
+            return
 
     def do_cd(self, arg: str):
         """change remote working directory"""
@@ -94,23 +94,26 @@ class Client(cmd.Cmd):
                     self.port = port
             elif 'int' in str(e):
                 print(f'{e} is invalid port')
-        finally:
-            print(self.host)
-            print(self.port)
-        if self.running:
+                return
+        try:
             self.control_sock.connect((self.host, self.port))
-            print(self.control_sock.recv(1024).decode())
+        except socket.gaierror as e:
+            print(str(e))
+            return
+        print(self.control_sock.recv(1024).decode())
+        self.do_system('')
+        self.do_user('')
 
     def _portmath(self, port):
-        """Calculating port coordinates. Accepts either a port number to be separated or a tuple of integers to be
-           calculated together."""
+        """Calculating port coordinates. Accepts either a port number to
+           be separated or a tuple of integers to be calculated together."""
         try:
             port = int(port)
         except (ValueError, TypeError):
             pass
         if isinstance(port, int):
             while True:
-                p2 = random.randint(5, 235)
+                p2 = randint(5, 235)
                 temp = port - p2
                 if temp % 256 == 0:
                     p1 = temp >> 8
@@ -120,12 +123,13 @@ class Client(cmd.Cmd):
                 port = list(map(int, port))
                 return (port[0] << 8) + port[1]
             except ValueError as e:
-                print(str(e))  # Need to setup proper exception handling back up the stack
+                # Need to setup proper exception handling back up the stack
+                print(str(e))
+                raise ValueError(e)
 
     def do_sendport(self, arg):
-        """toggle use of PORT cmd for each data connection. Not full implemented yet."""
-        if not self.running:
-            return
+        """toggle use of PORT cmd for each data connection.
+           Not full implemented yet."""
         if arg != '':
             if arg.lower() == 'on':
                 self._port_cmd = True
@@ -137,34 +141,117 @@ class Client(cmd.Cmd):
 
     def _create_data_conn(self):
         if self._port_cmd:
-            self._create_sock(active_ftp=True)
-            time.sleep(1)
-            ipadder = str(self.control_sock.getsockname()[0]).replace('.', ',')
-            ipadder += ',' + self._portmath(self.control_sock.getsockname()[1])
-            self.control_sock.send(bytes(f"PORT {ipadder}\r\n"))
+            while True:
+                try:
+                    self._create_sock(active_ftp=True)
+                    time.sleep(1)
+                    ipadder = str(self.control_sock.getsockname()[0]).replace('.', ',')  # noqa: E501
+                    ipadder += ',' + self._portmath(self.control_sock.getsockname()[1])  # noqa: E501
+                    break
+                except ValueError:
+                    pass
+            self.control_sock.send(bytes(f"PORT {ipadder}\r\n", 'utf-8'))
             print(self.control_sock.recv(1024).decode())
         else:
-            self.control_sock.send(b"PASV\r\n")
-            address = self.control_sock.recv(1024).decode().strip()
-            address = re.search(r'((\d{1,3},){4})(\d{1,3},\d{1,3})', address).groups()
-            port = (lambda x, y: (x * 256) + y)(*map(int, address[-1].split(',')))
-            self._create_sock(active_ftp=False, target=address[0], port=port)
+            address = None
+            while address is None:
+                self.control_sock.send(b"PASV\r\n")
+                address = self.control_sock.recv(1024).decode().strip()
+                regex = r'((\d{1,3},){4})(\d{1,3},\d{1,3})'
+                address = re.search(regex, address).groups()
+            port = (lambda x, y: (x * 256) + y)(*map(int, address[-1].split(',')))  # noqa: E501
+            address = address[0].replace(',', '.').rstrip('.')
+            self._create_sock(active_ftp=False, target=address, port=port)
 
     def _create_sock(self, active_ftp: bool, target=None, port: int = 0):
         self.data_sock = socket.socket()
         if active_ftp:
             while True:
                 try:
-                    self.data_sock.bind(('localhost', random.randint(40000, 60000)))
+                    self.data_sock.bind(('0.0.0.0', randint(40000, 60000)))
                     break
                 except OSError:
                     pass
         else:
             self.data_sock.connect((target, port))
 
+    def _file_stream(self, filename: str, get: bool = True, size: int = None):
+        """File movement function"""
+        chunk_size = 1024 * 1024
+        try:
+            if get:
+                # Code in get used to be here
+                if os.sep in filename:
+                    filename = filename.split(os.sep)[-1]
+                with tqdm.tqdm(total=size) as pbar:
+                    while True:
+                        with open(filename, 'wb') as f:
+                            chunk = self.data_sock.recv(chunk_size)
+                            if chunk == b'':
+                                break
+                            pbar.update(f.write(chunk))
+            else:
+                chunk_size = 65000
+                size = int(os.stat(filename).st_size)
+                with tqdm.tqdm(total=size, ascii='Upload') as pbar:
+                    while True:
+                        with open(filename, 'rb') as f:
+                            chunk = f.read(chunk_size)
+                            if chunk == b'':
+                                break
+                            pbar.update(self.data_sock.send(chunk))
+        except (FileNotFoundError, PermissionError, IsADirectoryError,
+                IOError) as e:
+            print(str(e))
+            self._abort()
+        finally:
+            self.data_sock.close()
+
+    def _data_stream(self):
+        chunk_size = 1024 * 1024
+        while True:
+            try:
+                conn = select.select([self.data_sock], [], [], 0.1)[0][0]
+                chunk = conn.recv(chunk_size)
+                if chunk == b'':
+                    break
+                print(chunk.decode())
+            except IndexError:
+                break
+        self.data_sock.close()
+
+    def _abort(self):
+        """Send ABORT signal"""
+        self.control_sock.send(b"ABOR\r\n")
+        self.control_sock.recv(1024)
+
+    def _data_connection(self, file=None, get=None, size: int = None):
+        if file:
+            self._file_stream(file, get, size)
+        else:
+            self._data_stream()
+
     def do_get(self, arg):
+        """receive file"""
+        self._create_data_conn()
+        self.control_sock.send(f"SIZE {arg}\r\n".encode())
+        size = int(self.control_sock.recv(1024).decode().strip().split()[-1])
         self.control_sock.send(bytes(f"RETR {arg}\r\n", 'utf-8'))
         print(self.control_sock.recv(1024).decode())
+        self._data_connection(file=arg, get=True, size=size)
+        print(self.control_sock.recv(1024).decode())
+
+    def do_put(self, arg):
+        """send one file"""
+        self._create_data_conn()
+        self.control_sock.send(f"STOR {arg}\r\n".encode())
+        print(self.control_sock.recv(1024).decode())
+        self._data_connection(file=arg)
+        print(self.control_sock.recv(1024).decode())
+
+    def do_send(self, arg):
+        """send one file"""
+        self.do_put(arg)
 
     def do_binary(self, arg):
         """set binary transfer type"""
@@ -182,7 +269,10 @@ class Client(cmd.Cmd):
 
     def do_ls(self, arg: str = ''):
         """list contents of remote directory"""
+        self._create_data_conn()
         self.control_sock.send(bytes(f"LIST {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+        self._data_connection()
         print(self.control_sock.recv(1024).decode())
 
     def do_dir(self, arg: str = ''):
@@ -196,14 +286,14 @@ class Client(cmd.Cmd):
 
     def do_bye(self, arg):
         """terminate ftp session and exit"""
-        if self.running:
-            self.do_disconnect('')
+        self.do_disconnect('')
 
     def do_close(self, arg):
         """terminate ftp session"""
-        self.control_sock.send(b"QUIT\r\n")
-        print(self.control_sock.recv(1024).decode())
-        self.control_sock.close()
+        if self.control_sock and self.control_sock.fileno() != -1:
+            self.control_sock.send(b"QUIT\r\n")
+            print(self.control_sock.recv(1024).decode())
+            self.control_sock.close()
 
     def do_disconnect(self, arg):
         """terminate ftp session"""
@@ -214,18 +304,45 @@ class Client(cmd.Cmd):
         self.control_sock.send(bytes(f"DELE {arg}\r\n", 'utf-8'))
         print(self.control_sock.recv(1024).decode())
 
+    def do_size(self, arg):
+        """show size of remote file"""
+        self.control_sock.send(bytes(f"SIZE {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
     def do_pwd(self, arg):
+        """print working directory on remote machine"""
         self.control_sock.send(b"PWD\r\n")
         print(self.control_sock.recv(1024).decode())
 
+    def do_mkdir(self, arg):
+        """create a directory on remote machine"""
+        self.control_sock.send(bytes(f"MKD {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_rmdir(self, arg):
+        """delete a directory on remote machine"""
+        self.control_sock.send(bytes(f"RMD {arg}\r\n", 'utf-8'))
+        print(self.control_sock.recv(1024).decode())
+
+    def do_rhelp(self, arg):
+        """get remote server help"""
+        self.control_sock.send(b"HELP\r\n")
+        print(self.control_sock.recv(1024))
+
     def do_quit(self, arg):
         """terminate ftp session"""
-        print('QUIT')
-        if self.running:
-            self.do_disconnect('')
+        self.do_disconnect('')
+        return True
+
+    def do_exit(self, arg):
+        """terminate ftp session"""
+        self.do_disconnect('')
         return True
 
 
 if __name__ == "__main__":
-    client = Client()
+    try:
+        client = Client(host=sys.argv[1])
+    except IndexError:
+        client = Client()
     client.cmdloop()
